eb87c847381c4534ef5ee1e82f8a643219d491bc
ajaymude <mudeajay@gmail.com>
2024-12-19 08:42:11 +0530

    array method is added

---
+++ ReactJS/ReduxToolkit.js
@@ -0,0 +1,84 @@
+// mutable premitive  , immutable non premitive
+
+// 1 steps  installation 
+// npm install redux react-redux
+
+
+
+
+
+// 
+//2 step  create  reducer.js
+const initialState = {
+    count: 0,
+  };
+  
+  const counterReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'INCREMENT':
+        return { ...state, count: state.count + 1 };
+      case 'DECREMENT':
+        return { ...state, count: state.count - 1 };
+      default:
+        return state;
+    }
+  };
+  
+  export default counterReducer;
+
+  
+// 3 step create store  
+import { createStore } from 'redux';
+import counterReducer from './reducer';
+
+const store = createStore(counterReducer);
+
+export default store;
+
+
+ // 4  use provider  index.js or App.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './App';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+
+// 5  connecting dispatch 
+
+
+
+
+// App.js
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+function App() {
+  const count = useSelector(state => state.count);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch({ type: 'INCREMENT' });
+  };
+
+  const decrement = () => {
+    dispatch({ type: 'DECREMENT' });
+  };
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
