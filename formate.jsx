1d68cf0fceece15868c2ec4c19e51a18a5ba1454
ajaymude <84218380+ajaymude@users.noreply.github.com>
2025-01-05 16:19:38 +0530

    Update formate.jsx

--- formate.jsx
+++ formate.jsx
@@ -18,6 +18,11 @@ app.get('/form', (req, res) => {
 
 app.post('/process', (req, res) => {
     res.send('Form submitted successfully');
+    
 });
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
+
+
+
+new for the fetch 
