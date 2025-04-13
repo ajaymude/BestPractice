483fa6e74ebc097a3ce607cb9f33b64fa6313b29
ajaymude <mudeajay@gmail.com>
2025-04-13 23:58:57 +0530

    dsa recursion

---
+++ 00-DSA/DSA01-Recursion.js
@@ -0,0 +1,27 @@
+
+
+// Recursion 
+// Recursion is when a function calls itself to solve smaller subproblems of a bigger problem.
+
+// ✅ Three Key Parts of Recursion:
+// Base Case – The condition when recursion stops.
+// Recursive Case – The part where the function calls itself.
+// Changing Input – Each call should move toward the base case.
+
+function recursiveFunction(parameters) {
+    if (baseCondition) {
+      return result; // 🔁 base case (stopping condition)
+    }
+    return recursiveFunction(smallerProblem); // 🌀 recursive case
+  }
+
+
+// Example 1: Print numbers from 1 to N
+function print1ToN(n) {
+    if (n === 0) return; // base case
+    print1ToN(n - 1);    // recursive call
+    console.log(n);      // print after the call
+  }
+  
+  print1ToN(5);
+  
