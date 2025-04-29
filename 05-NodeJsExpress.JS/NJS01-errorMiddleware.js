c8cf5d18f9434979ddc02c9ef5f46d4fc2c466e4
ajaymude <mudeajay@gmail.com>
2025-04-29 23:35:44 +0530

    error handle

--- 05-NodeJsExpress.JS/NJS01-errorMiddleware.js
+++ 05-NodeJsExpress.JS/NJS01-errorMiddleware.js
@@ -23,3 +23,200 @@ const asyncHandler = (fn) => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(next);
 
 export default asyncHandler;
+
+
+
+
+
+
+
+
+
+
+
+
+// errorHandler 
+
+// 
+
+// project setup 
+
+
+
+// db connnection 
+
+
+
+
+
+// 404 handler 
+const notFound = (req, res) => res.status(404).send('Route does not exist')
+
+module.exports = notFound
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+// error handler 
+
+// creating a error object 
+class CustomAPIError extends Error {
+  constructor(message, statusCode) {
+    super(message)
+    this.statusCode = statusCode
+  }
+}
+
+
+ CustomAPIError('message', 500) // we are calling the constructor of the class
+ // super is used to call the constructor of the parent class
+ // this.statusCode = statusCode // this is used to set the value of the statusCode property of the class 
+
+const createCustomError = (msg, statusCode) => {
+  return new CustomAPIError(msg, statusCode)
+}
+
+module.exports = { createCustomError, CustomAPIError }
+
+
+
+
+
+// if the user have 4 argument in the function it is mean it is a 
+// global error handler 
+
+const { CustomAPIError } = require('../errors/custom-error')
+const errorHandlerMiddleware = (err, req, res, next) => {
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ msg: err.message })
+  }
+  return res.status(500).json({ msg: 'Something went wrong, please try again' })
+}
+
+module.exports = errorHandlerMiddleware
+
+
+
+
+
+
+
+
+// async wrapper 
+// if we pass any any value in next() , it will call the global error handler 
+const asyncWrapper = (fn) => {
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+module.exports = asyncWrapper
+
+
+
+
+
+
+
+
+
+// controller with the async wrapper 
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({})
+  res.status(200).json({ tasks })
+})
+
+
+// controller with the error handler
+const getTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params
+  const task = await Task.findOne({ _id: taskID })
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404))
+  }
+
+  res.status(200).json({ task })
+})
+
+
+
+
+
+
+// route setup for the 
+
+const express = require('express')
+const router = express.Router()
+
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+  editTask,
+} = require('../controllers/tasks')
+
+router.route('/').get(getAllTasks).post(createTask)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+router.get('/' , funName);
+
+module.exports = router
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// make a list of the built in class in  the js 
+// what is the class in the js , why we use tthis , 
+// learn the every this about the class 
+// how to create the class 
+// what is the advance feature of the class 
+// what is the constructe in the class 
+// what is the super , this called constructor of the parent class 
+// what is the instance of the class 
