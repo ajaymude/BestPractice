a21c1f111109eec0c712ec454b5f0a9ecabc6a22
ajaymude <mudeajay@gmail.com>
2025-09-04 11:46:46 +0530

    figma master

---
+++ 01-Figam/00-syllabus.js
@@ -0,0 +1,45 @@
+// figma 
+
+// low fi wireframes
+// high fi wireframes
+// prototypes
+// design systems
+
+
+
+
+// frame 
+    //  you can change the frame size
+    //  you can change the background color
+    //  you can add grids and layout
+    //  you can add constraints to objects inside the frame
+    //  you can add auto layout to the frame
+
+
+
+
+// components
+// variants
+// auto layout
+// constraints
+// styles
+// plugins
+// collaboration
+// handoff
+// resources
+// community
+// figjam
+
+
+
+
+// you can select multiple objects by holding shift and clicking on them
+// you can group objects by selecting them and pressing ctrl + g
+// you can ungroup objects by selecting them and pressing ctrl + shift + g
+// you can duplicate objects by selecting them and pressing ctrl + d
+// you can delete objects by selecting them and pressing delete
+// you can copy objects by selecting them and pressing ctrl + c
+// you can paste objects by selecting the frame and pressing ctrl + v
+// you can undo actions by pressing ctrl + z
+// you can redo actions by pressing ctrl + shift + z
+// you can zoom in and out by pressing ctrl + + or ctrl + -
